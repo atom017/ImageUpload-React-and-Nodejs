90bd81040bd7b1aaa7918e59a2889d86c95dff4d
Antonio <khaingjar@gmail.com>
2022-01-22 20:30:25 +0630

    css changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import axios from 'axios';
 import {useState} from 'react'
 
 function App() {
-  const [imgInfo,setImgInfo] = useState({preview:'',data:null});
-
+  const [imgInfo,setImgInfo] = useState({preview:'',data:null,name:''});
+  
   const handleInputChange = (e) =>{
-    console.log('Input change  ',e.target.files);
+    console.log('Input change  ',e.target.files[0].name);
     const img={
       preview: URL.createObjectURL(e.target.files[0]),
-      data: e.target.files[0]
+      data: e.target.files[0],
+      name:e.target.files[0].name
     }
     console.log(img.preview);
     setImgInfo(img);
@@ -33,14 +34,16 @@ function App() {
       <h1>React image upload</h1>
       <div className="container">
         {imgInfo.preview && (<div className="img-container">
-          <img src={imgInfo.preview} alt="" />        
+          <img src={imgInfo.preview} alt="image" />  
+          <label className='imgLabel' >{imgInfo.name}</label>      
         </div>)}
         
         <form className='form' onSubmit={handleSubmit}>
-          
+          <label htmlFor="imageSelect" className='inputLabel'>Select Image</label>
           <input 
+          style={{display:'none'}}
           type="file" 
-          id="fileSelect" 
+          id="imageSelect" 
           accept='image/*'
           onChange={handleInputChange}/>
           <button type="sumbit" >Upload</button>
